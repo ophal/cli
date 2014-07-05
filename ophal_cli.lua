a0af4bcdb52e8def03b358ef316e8cb4bd709e29
Fernando Paredes Garcia <fernando@develcuy.com>
2014-07-05 16:44:04 -0500

    Add support for commands and command aliases.

--- ophal_cli.lua
+++ ophal_cli.lua
@@ -1,6 +1,34 @@
-local m = {
-  bootstrap = function ()
-  end
+local m; m = {
+
+  aliases = {},
+
+  build_aliases = function()
+    for _, command in pairs(m.commands) do
+      if command.alias ~= nil then
+        m.aliases[command.alias] = command
+      end
+    end
+  end,
+
+  bootstrap = function(arg)
+    local cmd, output
+    local commands, aliases = m.commands, m.aliases
+
+    -- TODO: detect and bootstrap Ophal.
+
+    m.build_aliases()
+
+    -- Execute command
+    cmd = commands[arg[1]]
+    alias = aliases[arg[1]]
+    if type(cmd) == 'table' then
+      output = cmd.exec()
+    elseif type(alias) == 'table' then
+      output = alias.exec()
+    end
+
+    print(output)
+  end,
 }
 
 return m
